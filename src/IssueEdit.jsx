08feb7dcfa6ee8906b69cd38f8be3411de902868
nguyenvandai61 <crystallifestyle05@gmail.com>
2019-06-09 13:51:59 +0700

    Horizontal Form

--- src/IssueEdit.jsx
+++ src/IssueEdit.jsx
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import NumInput from './NumInput.jsx';
 import DateInput from './DateInput.jsx';
-
+import { Card, Form, FormGroup, FormControl, ButtonToolbar, Button } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap'
 export default class IssueEdit extends React.Component { // eslint-disable-line
   constructor(props) {
     super(props);
@@ -30,7 +32,7 @@ export default class IssueEdit extends React.Component { // eslint-disable-line
 
   onChange(e, convertedValue) {
     const issue = Object.assign({}, this.state.issue);
-    const value = (convertedValue !== undefined) ? convertedValue : e.targe.value;
+    const value = (convertedValue !== undefined) ? convertedValue : e.target.value;
     issue[e.target.name] = value;
     this.setState({ issue });
   }
@@ -52,7 +54,7 @@ export default class IssueEdit extends React.Component { // eslint-disable-line
     fetch(`/api/issues/${this.props.match.params.id}`,
       {
         method: 'PUT',
-        headers: {'content-type': 'application/json'},
+        headers: { 'content-type': 'application/json' },
         body: JSON.stringify(this.state.issue)
       }
     ).then(response => {
@@ -81,7 +83,7 @@ export default class IssueEdit extends React.Component { // eslint-disable-line
           issue.created = new Date(issue.created);
           issue.completionDate = issue.completionDate != null ?
             new Date(issue.completionDate) : '';
-          issue.effort = (issue.effort)? issue.effort.toString() : '';
+          issue.effort = (issue.effort) ? issue.effort.toString() : '';
           this.setState({ issue: issue });
         })
       }
@@ -105,35 +107,73 @@ export default class IssueEdit extends React.Component { // eslint-disable-line
       </div>
       );
     return (
-      <div>
-        <form onSubmit={this.onSubmit}>
-          ID: {issue._id}
-          <br />
-          Created: {issue.created ? issue.created.toDateString() : ''}
-          <br />
-          Status: <select name="status" value={issue.status} onChange={this.onChange}>
-            <option value="Open">Open</option>
-            <option value="Assigned">Assigned</option>
-            <option value="New">New</option>
-          </select>
-          <br />
-          Owner: <input name="Owner" value={issue.owner} onChange={this.onChange} />
-          <br />
-          Effort: <NumInput size={5} name="effort" value={issue.effort} onChange={this.onChange}/>
+      <Card>
+        <Card.Header>Edit Issue</Card.Header>
+        <Form onSubmit={this.onSubmit}>
+          <FormGroup as={Row}>
+            <Col sm={3}>ID</Col>
+            <Col sm={9}> {issue._id}</Col>
+          </FormGroup>
+          <FormGroup as={Row}>
+            <Col sm={3}>Created</Col>
+            <Col sm={9}>{issue.created ? issue.created.toDateString() : ''}</Col>
+          </FormGroup>
+          <FormGroup as={Row}>
+            <Col sm={3}>Status</Col>
+            <Col sm={9}>
+              <FormControl as="select" name="status" value={issue.status} onChange={this.onChange}>
+                <option value="Open">Open</option>
+                <option value="Assigned">Assigned</option>
+                <option value="New">New</option>
+                <option value="Fixed">Fixed</option>
+                <option value="Verified">Verified</option>
+                <option value="Closed">Closed</option>
+              </FormControl>
+            </Col>
+          </FormGroup>
+          <FormGroup as={Row}>
+            <Col sm={3}>Owner</Col>
+            <Col sm={9}>
+            <FormControl name="Owner" value={issue.owner} onChange={this.onChange}/> 
+            </Col>
+          </FormGroup>
+          
+          
+          <FormGroup as={Row}>
+            <Col sm={3}>Effort</Col>
+            <Col sm={9}>
+            <FormControl as={NumInput} name="effort" value={issue.effort} onChange={this.onChange}/>
+            </Col>
+          </FormGroup>
+          <FormGroup as={Row}>
+           <Col sm={3}>Completion Date</Col>
+           <Col sm={9}>
+           <FormControl as={DateInput} value={issue.completionDate} onChange={this.onChange}
+            onValidityChange={this.onValidityChange}/>
+           
+           </Col>
+          </FormGroup>
+          
+          <FormGroup as={Row}>
+            <Col sm={3}>Title</Col>
+            <Col sm={9}>
+            <FormControl name="Title" value={issue.title} onChange={this.onChange}/>
+            </Col>
+          </FormGroup>
+          <FormGroup as={Row}>
+            <Col smOffset={3} sm={6}>
+              <ButtonToolbar>
+                <Button type="submit">Submit</Button>
+                <LinkContainer to="/issues"><Button>
+                    Back
+                  </Button></LinkContainer>
 
-          <br />
-          Completion Date: <DateInput name="completionDate" value={issue.completionDate} onChange={this.onChange}
-            onValidityChange={this.onValidityChange}
-          />
-          <br />
-          Title: <input name="Title" value={issue.title} onChange={this.onChange} />
-          <br />
+              </ButtonToolbar>
+            </Col>
+          </FormGroup>
           {validationMessage}
-          <button type="submit">Submit</button>
-          <br />
-          <Link to="/issues">Back to issue list</Link>
-        </form>
-      </div>
+        </Form>
+      </Card>
     );
   }
 }
